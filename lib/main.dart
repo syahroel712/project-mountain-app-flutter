444613a328cf4524b28eeb3c9cc32a1217bdd9e3
syahroel712 <syahroel712@gmail.com>
2022-01-25 02:05:31 +0700

    Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_mountain/pages/navpages/main_page.dart';
 import 'package:flutter_app_mountain/pages/welcome_page.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WelcomePage(),
+      home: MainPage(),
     );
   }
 }
